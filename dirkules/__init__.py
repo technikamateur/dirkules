49b73a7e493017af2037e4dc1b2947dc99877d9a
Daniel <technikamateur@gmx.de>
2019-03-04 18:54:41 +0100

    smart wird nun ausgelesen

--- dirkules/__init__.py
+++ dirkules/__init__.py
@@ -1,12 +1,10 @@
-import os
+import dirkules.config as config
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-baseDir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(
-    baseDir, 'dirkules.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object(config)
 db = SQLAlchemy(app)
 
 import dirkules.models
--- dirkules/driveManagement/driveController.py
+++ dirkules/driveManagement/driveController.py
@@ -5,6 +5,57 @@ import os
 
 
 def getAllDrives():
+    #vorbereitung
+    drives = []
+    driveDict = []
+    keys = ['device', 'name', 'smart']
+
+    blkid = subprocess.Popen(["hwinfo --disk --short"],
+                             stdout=subprocess.PIPE,
+                             shell=True,
+                             universal_newlines=True)
+    grepedDrives = subprocess.Popen(["grep", "/dev/sd"],
+                                    stdin=blkid.stdout,
+                                    stdout=subprocess.PIPE,
+                                    universal_newlines=True)
+    while True:
+        line = grepedDrives.stdout.readline()
+        if line != '':
+            drives.append(line.rstrip())
+        else:
+            break
+    blkid.stdout.close()
+    for line in drives:
+        values = []
+        line = line.replace(" ", "", 15)
+        values.append(line[:8])
+        values.append(line[8:])
+        values.append(smartPassed(values[0]))
+        driveDict.append(dict(zip(keys, values)))
+    return driveDict
+
+
+def smartPassed(device):
+    passed = False
+    smartctl = subprocess.Popen(
+        ["smartctl -H " + device],
+        stdout=subprocess.PIPE,
+        shell=True,
+        universal_newlines=True)
+    while True:
+        line = smartctl.stdout.readline()
+        if "PASSED" in line:
+            passed = True
+        elif line == '':
+            break
+        else:
+            pass
+    smartctl.stdout.close()
+    return passed
+
+
+#nicht verwenden
+def OLDgetAllDrives():
 
     #vorbereitung
     drives = []
@@ -39,14 +90,16 @@ def getAllDrives():
                 values.append("Was weiß ich...")
                 values.append(arrayline[3][6:-1])
                 values.append("Weiß ich auch noch nicht...")
-        elif any(not "LABEL" in s for s in arrayline) and any("UUID_SUB" in s for s in arrayline):
+        elif any(not "LABEL" in s
+                 for s in arrayline) and any("UUID_SUB" in s
+                                             for s in arrayline):
             values.append("Was weiß ich...")
             values.append(arrayline[3][6:-1])
-            values.append("Weiß ich auch noch nicht...")
+            values.append("(keiner)")
         else:
             values.append("Was weiß ich...")
             values.append(arrayline[2][6:-1])
-            values.append("Weiß ich auch noch nicht...")
+            values.append("(keiner)")
         #Dict für Jinja anfügen
         driveDict.append(dict(zip(keys, values)))
     return driveDict
