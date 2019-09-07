8f5c72d6282ac6d185b39557f2cf8edcd500017e
Daniel <daniel@technikamateur.de>
2019-09-07 13:15:56 +0200

    cleaning works

--- dirkules/manager/cleaning.py
+++ dirkules/manager/cleaning.py
@@ -1,14 +1,13 @@
 import os
 from dirkules.models import Cleaning
 from dirkules.hardware import autoclean
-from dirkules import db
+from dirkules import db, app
 
 
 def clean_folders():
     for folder in Cleaning.query.all():
         if folder.state and os.path.isdir(folder.path):
             result = autoclean.autoclean(folder.path)
-            print(result)
-            print(result[0])
-            print(result[1])
+        elif not os.path.isdir(folder.path):
+            app.logger.warning('folder not found: %s', folder.path)
     db.session.commit()
