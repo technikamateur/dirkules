1a1207fbda05ead5664b37a835d9b24beaf17005
Daniel <daniel@technikamateur.de>
2019-09-12 14:36:07 +0200

    removed Telegram Com

--- dirkules/__init__.py
+++ dirkules/__init__.py
@@ -3,7 +3,6 @@ import dirkules.config as config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_apscheduler import APScheduler
-import dirkules.TelegramCom
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -13,8 +12,6 @@ import dirkules.models
 
 # create db if not exists
 db.create_all()
-# start communication
-communicator = TelegramCom.TelegramCom(app)
 # start scheduler
 scheduler = APScheduler()
 scheduler.init_app(app)
