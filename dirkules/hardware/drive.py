3b85a90956d3753a0d63cddca8e6b9c95aa88005
Daniel <daniel@technikamateur.de>
2019-10-17 18:27:56 +0200

    fixed bug long mountpoints

--- dirkules/hardware/drive.py
+++ dirkules/hardware/drive.py
@@ -96,11 +96,11 @@ def part_for_disk(device):
     del parts[0]
     for part in parts:
         values = list()
-        for start, end in zip(element_length, element_length[1:]):
-            if end == element_length[-1:]:
+        for start, next_start in zip(element_length, element_length[1:]):
+            if next_start == element_length[-1:][0]:
                 values.append(part[start:len(part)].strip())
             else:
-                values.append(part[start:(end - 1)].strip())
+                values.append(part[start:(next_start - 1)].strip())
         part_dict.append(dict(zip(keys, values)))
 
     return part_dict
