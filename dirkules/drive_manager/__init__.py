13ba4382912e5d2d369df483bb4b3194ac4d0799
Daniel <daniel@technikamateur.de>
2019-12-27 16:08:06 +0100

    mores drive manager features

--- dirkules/drive_manager/__init__.py
+++ dirkules/drive_manager/__init__.py
@@ -4,9 +4,11 @@ import subprocess
 
 class DriveManager:
     """
-    There should be only one DriveManager in your app.
+    There should be only one DriveManager in your app!
     """
     smartctl_available = False
+    drive_keys = ['name', 'model', 'serial', 'size', 'rota', 'rm', 'hotplug', 'state', 'smart']
+    part_keys = ['size', 'name', 'label', 'fs', 'uuid', 'mount']
 
     def __init__(self):
         self.sorted_drive_dict = None
@@ -39,10 +41,6 @@ class DriveManager:
 
     def _update_drives(self):
         drive_dict = list()
-        keys = [
-            'name', 'model', 'serial', 'size', 'rota', 'rm', 'hotplug', 'state',
-            'smart'
-        ]
         lsblk = subprocess.run(["sudo", "lsblk", "-I", "8", "-d", "-b", "-o", "NAME,MODEL,SERIAL,SIZE,ROTA,RM,HOTPLUG"],
                                shell=False, timeout=45, check=True, universal_newlines=True, stdout=subprocess.PIPE)
         drives = lsblk.stdout.splitlines()
@@ -54,7 +52,7 @@ class DriveManager:
                 new_line[1] = new_line[1] + " " + new_line[2]
                 del new_line[2]
             values = []
-            for i in range(len(keys) - 2):
+            for i in range(len(self.drive_keys) - 2):
                 if new_line[i] == "0":
                     values.append(False)
                 elif new_line[i] == "1":
@@ -63,13 +61,54 @@ class DriveManager:
                     values.append(new_line[i])
             values.append("running")
             values.append(self._is_healthy("/dev/" + values[0]))
-            drive_dict.append(dict(zip(keys, values)))
+            drive_dict.append(dict(zip(self.drive_keys, values)))
         self.sorted_drive_dict = sorted(drive_dict, key=lambda drive: drive['name'])
 
-    def get_all_drives(self):
+    def get_all_drives(self, cached=False):
         """
         Returns a standardized list of dictionaries which contains all information about all drives in the system.
+        If cached is true it will return a cached version of the drives.
+        :type cached: bool
         :rtype: list
         """
-        self._update_drives()
+        if not cached or self.sorted_drive_dict is None:
+            self._update_drives()
         return self.sorted_drive_dict
+
+    def part_for_disk(self, device):
+        """
+        This function is used to return a standardized list of dictionaries which contains all information about
+        this partition
+        :param device: A device e.g. "/dev/sda"
+        :type device: str
+        :return: list of dictionaries (every dictionary represents a partition)
+        :rtype: list
+        """
+        # lsblk /dev/sdd -l -b -o NAME,LABEL,FSTYPE,SIZE,UUID,MOUNTPOINT
+        part_dict = list()
+        lsblk = subprocess.run(["sudo", "lsblk", device, "-l", "-b", "-o", "SIZE,NAME,LABEL,FSTYPE,UUID,MOUNTPOINT"],
+                               shell=False, timeout=45, check=True, universal_newlines=True, stdout=subprocess.PIPE)
+        parts = lsblk.stdout.splitlines()
+        del parts[1]
+        element_length = list()
+        counter = 0
+        pre_value = " "
+        for char in parts[0]:
+            if char != " " and pre_value == " ":
+                if len(element_length) == 0:
+                    element_length.append(0)
+                else:
+                    element_length.append(counter)
+            counter += 1
+            pre_value = char
+        element_length.append(len(parts[0]))
+        del parts[0]
+        for part in parts:
+            values = list()
+            for start, next_start in zip(element_length, element_length[1:]):
+                if next_start == element_length[-1:][0]:
+                    values.append(part[start:len(part)].strip())
+                else:
+                    values.append(part[start:(next_start - 1)].strip())
+            part_dict.append(dict(zip(self.part_keys, values)))
+        return part_dict
